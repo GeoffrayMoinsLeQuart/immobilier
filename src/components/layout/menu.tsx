9412ac86fda800e56eb8aa507c93d8b1fbdeaac3
geoffray <geoffray.atlan@gmail.com>
2023-10-20 00:07:36 +0200

    modification

--- src/components/layout/menu.tsx
+++ src/components/layout/menu.tsx
@@ -6,7 +6,7 @@ const Menu: FC = () => {
 	return (
 		<>
 			<span className='max-xl:hidden flex justify-center items-center gap-8'>
-				<NavLink href='/faq'>'header.faq'</NavLink>
+				<NavLink href='/faq'>header.faq</NavLink>
 				<NavLink href='/profil'>
 					<Image src='/icons/profil.png' width={20} height={20} alt='Snow' className='' />
 				</NavLink>
